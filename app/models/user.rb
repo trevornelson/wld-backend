8087c08580e7babcffbfc54cfae9eaa791913113
Trevor Nelson <trevor.nelson1@gmail.com>
2017-03-11 18:17:52 -0500

    Add core values on user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
 
+	after_create :generate_core_values
 	before_save :downcase_email
 
 	validates_presence_of :email, :first_name, :last_name, :password
@@ -17,12 +18,17 @@ class User < ApplicationRecord
 	has_many :quarterly_todos
 	has_many :daily_todos
 
-	def downcase_email
-		self.email = self.email.delete(' ').downcase
-	end
-
 	def as_json(options={})
 		super(options.merge({ except: [:password_digest] }))
 	end
+
+	private
+		def generate_core_values
+			3.times { values.create(content: '') }
+		end
+
+		def downcase_email
+			self.email = self.email.delete(' ').downcase
+		end
 
 end
